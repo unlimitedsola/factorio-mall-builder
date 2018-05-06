322222a440737948e94b103783b9540c1469d25d
Sola <dev@sola.love>
2018-05-07 05:17:42 +0800

    add weighted edge
    Signed-off-by: Sola <dev@sola.love>

--- src/main/kotlin/love/sola/factorio/mallbuilder/Visualization.kt
+++ src/main/kotlin/love/sola/factorio/mallbuilder/Visualization.kt
@@ -1,5 +1,6 @@
 package love.sola.factorio.mallbuilder
 
+import guru.nidi.graphviz.attribute.Label
 import guru.nidi.graphviz.attribute.Shape
 import guru.nidi.graphviz.engine.Format
 import guru.nidi.graphviz.engine.Graphviz
@@ -10,11 +11,17 @@ import javafx.scene.image.Image
 fun genDAG(recipes: List<Recipe>): Image {
     var graph = Factory.graph("Visualization")
         .directed().strict()
-    val nodes = recipes.flatMap { it.ingredients }.map { it.name }.distinct()
+    val nodes = recipes.flatMap { it.ingredients.map { it.name } + it.products.map { it.name } }.distinct()
         .associateTo(hashMapOf()) { it to Factory.node(it).with(Shape.RECTANGLE) }
     for (recipe in recipes) {
-        recipe.ingredients.forEach {
-            nodes[it.name] = nodes[it.name]?.link(*recipe.products.map { it.name }.toTypedArray())
+        recipe.ingredients.forEach { ingredient ->
+            recipe.products.forEach { product ->
+                nodes[ingredient.name] = nodes[ingredient.name]
+                    ?.link(
+                        Factory.to(nodes[product.name])
+                            .with(Label.of("${(ingredient.amount / recipe.energy) * 60}/min"))
+                    )
+            }
         }
     }
     graph = graph.with(*nodes.values.toTypedArray())
