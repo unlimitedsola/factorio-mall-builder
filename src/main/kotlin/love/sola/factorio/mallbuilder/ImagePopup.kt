99d111270d178fcdc4f4009585deef167cd1e93a
Sola <dev@sola.love>
2018-05-07 22:12:13 +0800

    add "Save as" and "Copy"
    Signed-off-by: Sola <dev@sola.love>

--- src/main/kotlin/love/sola/factorio/mallbuilder/ImagePopup.kt
+++ src/main/kotlin/love/sola/factorio/mallbuilder/ImagePopup.kt
@@ -1,18 +1,37 @@
 package love.sola.factorio.mallbuilder
 
+import javafx.embed.swing.SwingFXUtils
 import javafx.scene.Parent
 import javafx.scene.image.Image
-import tornadofx.Fragment
-import tornadofx.borderpane
-import tornadofx.center
-import tornadofx.imageview
+import javafx.scene.input.DataFormat
+import javafx.stage.FileChooser
+import tornadofx.*
+import javax.imageio.ImageIO
 
 class ImagePopup : Fragment() {
     val image: Image by param()
     override val root: Parent = borderpane {
         center {
             imageview(image) {
-                autosize()
+                isPreserveRatio = true
+                fitToParentSize()
+                contextmenu {
+                    item("Copy") {
+                        action {
+                            clipboard.put(DataFormat.IMAGE, image)
+                        }
+                    }
+                    item("Save as") {
+                        action {
+                            val file = chooseFile(
+                                "Save",
+                                mode = FileChooserMode.Save,
+                                filters = arrayOf(FileChooser.ExtensionFilter("Image", "*.png"))
+                            ).single()
+                            ImageIO.write(image.let { SwingFXUtils.fromFXImage(it, null) }, "png", file)
+                        }
+                    }
+                }
             }
         }
     }
